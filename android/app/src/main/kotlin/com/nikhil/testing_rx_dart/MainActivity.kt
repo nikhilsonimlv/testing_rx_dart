2c37f0c4bfa9e42de49a06edf8f919c779e78ed3
Nikhil Soni <nikhil.soni@bottomline.com>
2023-03-09 07:05:32 +0530

    Step 1

---
+++ android/app/src/main/kotlin/com/nikhil/testing_rx_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nikhil.testing_rx_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rxdart/rxdart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends HookWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final subject = useMemoized(() => BehaviorSubject<String>(), [key]);
+    //dispose old subject
+    useEffect(() => subject.close, [subject]);
+    return Scaffold(
+      appBar: AppBar(
+        title: StreamBuilder<String>(builder: (context, snapshot) => Text(snapshot.data??""), stream: subject.stream.distinct().debounceTime(const Duration(seconds: 1,)),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: subject.sink.add,
+        ),
+      ),
+    );
+  }
+}
